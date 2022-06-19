b23f7a3bd13d56d58482e7e25081dbf2d40c1130
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-19 17:23:29 +0800

    Delete core directory

--- core/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import PublicIP, PublicPort, PublicDomain, CloudCluster, ContainerImage, StoresContainerImage, RunsContainerImage, GithubMirror
-
-__all__ = ["PublicDomain", "PublicIP", "CloudCluster", "PublicPort", "ContainerImage", "StoresContainerImage", "RunsContainerImage", "GithubMirror"]
