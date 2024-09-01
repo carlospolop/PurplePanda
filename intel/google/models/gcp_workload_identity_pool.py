9b7bf5b0088d718f54a3c0d6188915ef97d08b5e
Carlos Polop <carlospolop@gmail.com>
2024-09-01 18:45:17 +0200

    Fix workforcePools issue

--- intel/google/models/gcp_workload_identity_pool.py
+++ intel/google/models/gcp_workload_identity_pool.py
@@ -16,13 +16,19 @@ class GcpWorkloadIdentityPool(GcpPrincipal):
     def __init__(self, name, *args, **kwargs):
         kwargs["name"] = name
 
-        name_part = name.split("/projects/")[1]
-        kwargs["project_number"] = name_part.split("/")[0]
-
-        name_part = name.split("/workloadIdentityPools/")[1]
-        kwargs["pool_id"] = name_part.split("/")[0]
-
-        kwargs["final_part"] = name.split(f"/workloadIdentityPools/{kwargs['pool_id']}/")[1]
+        if "/projects/" in name:
+            name_part = name.split("/projects/")[1]
+            kwargs["project_number"] = name_part.split("/")[0]
+        
+        if "/workloadIdentityPools/" in name:
+            name_part = name.split("/workloadIdentityPools/")[1]
+            kwargs["pool_id"] = name_part.split("/")[0]
+            kwargs["final_part"] = name.split(f"/workloadIdentityPools/{kwargs['pool_id']}/")[1]
+        
+        elif "/workforcePools/" in name:
+            name_part = name.split("/workforcePools/")[1]
+            kwargs["pool_id"] = name_part.split("/")[0]
+            kwargs["final_part"] = name.split(f"/workforcePools/{kwargs['pool_id']}/")[1]
 
         super().__init__(*args, **kwargs)
         self.gcp = True
