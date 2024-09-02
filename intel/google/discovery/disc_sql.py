ba70076f738a2a7f4cb922e001fb970edd5d8fc2
Carlos Polop <carlospolop@gmail.com>
2024-09-02 15:30:11 +0200

    asd

--- intel/google/discovery/disc_sql.py
+++ intel/google/discovery/disc_sql.py
@@ -44,6 +44,8 @@ class DiscSql(GcpDisc):
             inst_obj.save()
 
             for ip in i["ipAddresses"]:
+                if not ip["ipAddress"]:
+                    _ = 1
                 ip_obj = PublicIP(ip=ip["ipAddress"]).save()
                 inst_obj.public_ips.update(ip_obj)
                 inst_obj.save()
